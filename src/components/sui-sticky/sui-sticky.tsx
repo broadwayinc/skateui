e53adf64feef8b17c2e1341084f21a6a7e15ee14
jinyoon <seejwy@users.noreply.github.com>
2022-07-06 10:49:02 +0900

    Modify sui-sticky for smoother animation Making use of requestAnimationFrame instead of manually throttling the speed of requests

--- src/components/sui-sticky/sui-sticky.tsx
+++ src/components/sui-sticky/sui-sticky.tsx
@@ -10,18 +10,18 @@ export class SuiSticky {
   @State() sticky: HTMLElement;
   @State() isSticky = true;
   @State() oldPosition = 0;
-  @State() newPosition= 0;
+  @State() newPosition = 0;
   @State() positionY = 0;
 
-  @Listen('scroll', {target: 'window'})
-  async scroll() {
-    if(this.sticky && this.isSticky) {
+  @Listen('scroll', { target: 'window' })
+  scroll() {
+    if (this.sticky && this.isSticky) {
       this.setPosition();
       this.newPosition = this.getYPosition();
-      if(this.newPosition > this.oldPosition) {
-        await this.setTransform('down');
+      if (this.newPosition > this.oldPosition) {
+        this.setTransform('down');
       } else {
-        await this.setTransform('up');
+        this.setTransform('up');
       }
       this.oldPosition = this.getYPosition();
     }
@@ -31,20 +31,25 @@ export class SuiSticky {
     return Math.floor(window.scrollY);
   }
 
-  async setTransform(direction: string) {
-    return new Promise(resolve => {
-      if((direction === 'down' && this.positionY > this.sticky.clientHeight * -1) || (direction === 'up' && this.positionY <= 0)) {
-        this.positionY -= (this.newPosition - this.oldPosition) * 0.6;
+  setTransform(direction: string) {
+    let { newPosition, oldPosition } = this;
+    if (direction === 'down' && this.positionY > this.sticky.clientHeight * -1) {
+      window.requestAnimationFrame(() => {
+        this.positionY -= (newPosition - oldPosition) * 0.6;
+      });
+      if (this.positionY < this.sticky.clientHeight * -1) {
+        this.positionY = this.sticky.clientHeight * -1;
       }
-      if(this.positionY <= this.sticky.clientHeight * -1) this.positionY = (this.sticky.clientHeight + 1) * -1;
-      if(this.positionY > 0) this.positionY = 0;
-
-      resolve(true);
-    })
+    } else if (direction === 'up') {
+      window.requestAnimationFrame(() => {
+        this.positionY += (oldPosition - newPosition) * 0.6;
+      });
+    }
+    if (this.positionY > 0) this.positionY = 0;
   }
 
   setPosition() {
-    if(this.host.getBoundingClientRect().y <= 0) {
+    if (this.host.getBoundingClientRect().y <= 0) {
       this.isSticky = true;
     } else {
       this.isSticky = false;
@@ -60,7 +65,7 @@ export class SuiSticky {
   render() {
     return (
       <Host>
-        <div class="sticky" ref={(el) => this.sticky = el as HTMLInputElement} style={{position: this.isSticky ? 'sticky' : 'relative', transform: `translateY(${this.positionY}px)`}}>
+        <div class="sticky" ref={(el) => this.sticky = el} style={{ position: this.isSticky ? 'sticky' : 'relative', transform: `translateY(${this.positionY}px)` }}>
           <slot></slot>
         </div>
       </Host>
