eb3dbd82e1c3b8eb637e1c1cbc903c7ac75caa73
Baksa Gimm <baksa@broadwayinc.com>
2022-12-13 13:33:16 +0900

    deploy script update

--- deploy.py
+++ deploy.py
@@ -8,25 +8,26 @@ ret = os.system('git pull')
 if ret != 0:
     print('==Failed to pull==')
 
+with open('./package.json', 'r') as p:
+    package = json.loads(p.read())
+
 
 def publish():
-    with open('./package.json', 'r') as p:
-        package = json.loads(p.read())
 
-        ret = os.system('npm publish')
+    ret = os.system('npm publish')
 
-        if ret != 0:
-            print('==Failed to publish==')
+    if ret != 0:
+        print('==Failed to publish==')
 
-        else:
-            distribute(package['version'])
+    else:
+        distribute()
 
     print('==END==')
 
 
-def distribute(version):
+def distribute():
     ret = os.system(
-        f'aws s3 sync ./dist/skateui s3://broadwayinc.dev/jslib/skateui/{version} --acl public-read')
+        f"aws s3 sync ./dist/skateui s3://broadwayinc.dev/jslib/skateui/{package['version']} --acl public-read")
 
     if ret != 0:
         print('==Failed to upload==')
