759fe3508fde150203f0eaf890499d572d44da74
baksagimm <baksa@broadwayinc.com>
2023-01-12 20:31:30 +0900

    fixed sui-select option colors

--- src/components/sui-select/sui-select.tsx
+++ src/components/sui-select/sui-select.tsx
@@ -94,6 +94,10 @@ export class SuiSelect {
           return val;
         });
 
+        if (!this.isMultiple) {
+          this.el.style.setProperty('opacity', '0', 'important');
+        }
+
         if (!needAdjustment) {
           this.el.style.setProperty('margin', '0', 'important');
           return;
@@ -119,8 +123,6 @@ export class SuiSelect {
           padding.map(p => {
             return p ? `-${p}px` : '0px';
           }).join(' '), 'important');
-
-        this.el.style.setProperty('padding-right', `calc(${padding[1]}px + 1em)`, 'important');
       },
       appendIdToSlotElement: true,
       excludeAttribute: this.isMultiple ? ['value'] : ['size', 'value'] // size attribute should not work for multiple select
@@ -147,7 +149,7 @@ export class SuiSelect {
             <path d="m81.957 144.91 252.97 305.17c4.7695 5.293 10.496 7.9336 17.16 7.9336 6.1875 0 11.676-2.6445 16.438-7.9453l250.12-305.17c6.1875-8.4844 7.3984-17.746 3.5742-27.82-3.8008-10.051-10.703-15.094-20.727-15.094l-202.93 0.003906h-300.16c-9.5352 0-16.438 5.0391-20.727 15.094-3.8008 10.078-2.3672 19.355 4.2852 27.828z" />
           </svg>
           <slot></slot>
-          {/* <span data-selected={this.valueDisplay} style={{ display: this.isMultiple ? 'none' : 'flex', width: `calc(100% - ${this.isMultiple ? 0 : 0.75}em)` }}></span> */}
+          <span data-selected={this.valueDisplay} style={{ display: this.isMultiple ? 'none' : 'flex', width: `calc(100% - ${this.isMultiple ? 0 : 0.75}em)` }}></span>
         </div>
       </Host>
     );
